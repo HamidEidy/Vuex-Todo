1839603769cd083a42af0b7c4c57e11c6031c901
Hamidreza Eidy <hamidreza.eidy1999@gmail.com>
2023-12-10 13:40:22 +0330

    Add files via upload

---
+++ src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 text-center mt-5">
+        <h1>Vuex Tutorial</h1>
+        <p class="mt-4">
+         The project is about task management with the ability to add,<br>
+          edit, filter and delete tasks. receiving data from an API using the axios package.<br>
+          The code is written using the Vue framework with a focus on using the Vuex state manager.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
